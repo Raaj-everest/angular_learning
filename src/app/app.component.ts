d307cf5b9de3f5215a5daba7fff662be9144a406
Rajesh <Rajesh@hometriangle.com>
2022-03-12 16:56:18 +0530

    navigation with event binding is done for the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  feature='recipe';
   title = 'recipe_app';
+
+  selectedFeature(feature:string){
+      this.feature=feature;
+  }
 }
