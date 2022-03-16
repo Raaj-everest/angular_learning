79afd5a83a401c63b2537b7045849e39f1fb7b7d
Rajesh <Rajesh@hometriangle.com>
2022-03-16 18:30:18 +0530

    recipe-edit feature added

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeEditComponent } from '../recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from '../recipes/recipe-start/recipe-start.component';
 import { RecipeDiscriptionComponent } from '../recipes/recipes-list/recipe-discription/recipe-discription.component';
 import { RecipesComponent } from '../recipes/recipes.component';
@@ -11,8 +12,10 @@ const appRoutes: Routes = [
     path: 'recipes',
     component: RecipesComponent,
     children: [
-      { path: '', component: RecipeStartComponent, },
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent },
       { path: ':id', component: RecipeDiscriptionComponent },
+      { path: ':id/edit', component: RecipeEditComponent },
     ],
   },
   { path: 'shoppingList', component: ShoppingListComponent },
